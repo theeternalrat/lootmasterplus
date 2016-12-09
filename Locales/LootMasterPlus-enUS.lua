97ff61e7e02a59ce20bf1f7f5d8d2bb5a65c5b80
atkinson137 <atkinson21@gmail.com>
2016-12-08 23:05:25 -0800

    Started the base of the addon

---
+++ Locales/LootMasterPlus-enUS.lua
@@ -0,0 +1,5 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("LootMasterPlus", "enUS", true)
+
+if L then
+    L["MyMessage"] = "My Message Locale Test"
+end
---
+++ LootMasterPlusCore.lua
@@ -0,0 +1,56 @@
+LMP = LibStub("AceAddon-3.0"):NewAddon("LootMasterPlus", "AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0")
+
+--localization
+local L = LibStub("AceLocale-3.0"):GetLocale("LootMasterPlus", true)
+
+function LMP:OnInitialize()
+    --called when addon is first loaded in-game
+    self.db = LibStub("AceDB-3.0"):New("LootMasterPlusDB")
+    --TODO: profiles with default
+end
+
+function LMP:OnEnable()
+    --called when addon is enabled
+end
+
+function LMP:OnDisable()
+    --called when addon is disabled
+end
+
+--options table for Ace3
+local options = {
+    name = "LootMasterPlus",
+    handler = LMP,
+    type = 'group',
+    args = {
+        msg = {
+            type = 'input',
+            name = L["MyMessage"],
+            desc = 'The message for my addon',
+            set = 'SetMyMessage',
+            get = 'GetMyMessage'
+        }
+    }
+}
+
+--setup profiles for options
+options.args.profile = LibStub("AceDBOptions-3.0"):GetOptionsTable(db)
+
+--register the options table
+LibStub("AceConfig-3.0"):RegisterOptionsTable("LootMasterPlus", options, {"lmp", "lootmasterplus"})
+
+--chat commands
+function LMP:GetMyMessage(info)
+    return myMessageVar
+end
+
+function LMP:SetMyMessage(info, input)
+    myMessageVar = input
+end
+
+--AceComm inter-addon comm
+LMP:RegisterComm("LMP$$")
+
+function LMP:OnCommReceived(prefix, message, dist, sender)
+    --process the message
+end
